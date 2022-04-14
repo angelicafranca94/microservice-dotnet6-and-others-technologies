5a653ec93b712cedb6a33de6a2111997edfd6c22
angelicafranca94 <gefranca94@gmail.com>
2022-04-14 11:59:35 -0300

    Criando a Context

---
+++ Microservice/Catalog.API/Data/CatalogContextSeed.cs
@@ -0,0 +1,52 @@
+﻿using Catalog.API.Entities;
+using MongoDB.Driver;
+
+namespace Catalog.API.Data;
+
+public class CatalogContextSeed
+{
+    public static void SeedData(IMongoCollection<Product> productCollection)
+    {
+        bool existsProduct = productCollection.Find(p => true).Any();
+
+        if (!existsProduct)
+            productCollection.InsertManyAsync(GetMyProducts());
+    }
+
+    private static IEnumerable<Product> GetMyProducts()
+    {
+        return new List<Product>()
+        {
+            new Product()
+            {
+                Id = "1",
+                Name = "Iphone X",
+                Description = "Phone Apple Industries",
+                Image= "product1.png",
+                Price = 11.000M,
+                Category = "Phone Mobile"
+                
+            },
+            new Product()
+            {
+                Id = "2",
+                Name = "OnePlus8T",
+                Description = "Phone Oneplus Industries",
+                Image= "product1.png",
+                Price = 3.000M,
+                Category = "Phone Mobile"
+
+            },
+             new Product()
+            {
+                Id = "3",
+                Name = "Travasumg",
+                Description = "Phone Samsung Industries",
+                Image= "product1.png",
+                Price = 100M,
+                Category = "Phone Mobile"
+
+            }
+        };
+    }
+}
