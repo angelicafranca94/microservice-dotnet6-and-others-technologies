3052a76058674d2fbe19b98ca568511680059838
angelicafranca94 <gefranca94@gmail.com>
2022-04-18 10:46:30 -0300

    Implementando a interface, criando a controller, injetando dependencia.

--- Basket.API/Program.cs
+++ Basket.API/Program.cs
@@ -1,3 +1,5 @@
+using Basket.API.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,21 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+#region BD
+
+builder.Services.AddStackExchangeRedisCache(options =>
+{
+    options.Configuration = builder.Configuration.GetValue<string>("CacheSettings:ConnectionString");
+});
+
+#endregion
+
+#region Dependence Injection
+
+builder.Services.AddScoped<IBasketRepository, BasketRepository>();
+
+#endregion
 
 var app = builder.Build();
 
