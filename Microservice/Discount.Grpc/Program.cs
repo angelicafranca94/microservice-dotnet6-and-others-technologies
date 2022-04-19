fa0f3631dd8f96e21e63a4add494a5f147910ff5
angelicafranca94 <gefranca94@gmail.com>
2022-04-19 15:29:57 -0300

    Criando o repositorio grpc

--- Microservice/Discount.Grpc/Program.cs
+++ Microservice/Discount.Grpc/Program.cs
@@ -1,5 +1,6 @@
 
 using Discount.Grpc.Repositories;
+using Discount.Grpc.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -15,10 +16,16 @@ builder.Services.AddScoped<IDiscountRepository, DiscountRepository>();
 
 #endregion
 
+#region AutoMapper
+
+builder.Services.AddAutoMapper(typeof(Program));
+
+#endregion
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-//app.MapGrpcService<GreeterService>();
+app.MapGrpcService<DiscountService>();
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
 app.Run();
