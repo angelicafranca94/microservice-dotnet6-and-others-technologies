b001401e5ebe264c1a49ceaad9a2ce68695c232c
angelicafranca94 <gefranca94@gmail.com>
2022-04-19 15:10:38 -0300

    Gerando o arquivo proto

--- Microservice/Discount.Grpc/Program.cs
+++ Microservice/Discount.Grpc/Program.cs
@@ -1,4 +1,5 @@
-using Discount.Grpc.Services;
+
+using Discount.Grpc.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,10 +9,16 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddGrpc();
 
+#region Dependence Injection
+
+builder.Services.AddScoped<IDiscountRepository, DiscountRepository>();
+
+#endregion
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-app.MapGrpcService<GreeterService>();
+//app.MapGrpcService<GreeterService>();
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
 app.Run();
